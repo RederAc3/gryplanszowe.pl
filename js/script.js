278f893729846c15188b443efee034dd96566e08
Andrzej Kot <reder.ac3@gmail.com>
2019-03-10 22:43:27 +0100

    Bugs fix jQuery

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
+// ##############--PRELOADER--################
+
 $(window).load(function () {
     $("#loader-wrapper").fadeOut();
 });
+
+// ####################--SCROOL-BUTTON--#####################
+
+jQuery(function ($) {
+    //scroll reset
+    $.scrollTo(0);
+
+    $('#link1').click(function () { $.scrollTo($('#history'), 500);});
+    $('#scrollup').click(function () { $.scrollTo($('body'), 1000); });
+}
+);
+
+//show while scrolling
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 500) $('#scrollup').fadeIn();
+    else $('#scrollup').fadeOut();
+}
+);
+
