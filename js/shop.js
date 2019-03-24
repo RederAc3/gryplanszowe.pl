8751da42639806b3b3b798954e0ddbaa2bf07846
Andrzej Kot <reder.ac3@gmail.com>
2019-03-24 21:18:18 +0100

    Add setTimeout

--- js/shop.js
+++ js/shop.js
@@ -2,7 +2,8 @@ var prince = document.querySelectorAll('.prince-number');
 var buy = document.querySelectorAll('.buy-button');
 var title = document.querySelectorAll('.title-product');
 var modal = document.getElementById('modal');
-
+var scrollTop = document.documentElement.scrollTop;
+console.log(scrollTop);
 // WYWALA NON STOP NaN NWM CO Z TYM ZROBIĆ
 // var allPrince = 0;
 var getInfo = function() {
@@ -21,14 +22,19 @@ var getInfo = function() {
             console.log(this.previousElementSibling.innerText.replace('zł', ''));
             console.log(this.nextElementSibling.innerText.trim());
 
-            modal.classList.remove('none');
+            modal.classList.remove('modal-none');
             modal.innerHTML = '<div id="exit"></div>' + '<p>Dodano do koszyka: ' + addTitle + '</p>' + addPrince + '<p>Razem: ' + typeof(allPrince) +'zł </p>';
-            
+
             var exitModal = document.getElementById('exit');
 
             exitModal.addEventListener('click', function() {
-                modal.classList.add('none');
+                modal.classList.add('modal-none');
             });
+
+
+            setTimeout(function() {
+                modal.classList.add('modal-none');
+            }, 3000);
             // return allPrince;
         });
     }
