ca1c6749da7fea8ad9cdae16357c3e3e0da14b81
Andrzej Kot <reder.ac3@gmail.com>
2019-03-25 20:28:42 +0100

    Add translate

--- js/eng.js
+++ js/eng.js
@@ -1,19 +1,37 @@
+// ####-SECTION--NAV-####
+
+document.getElementById('nav-title-1').innerHTML = 'BOARD';
+document.getElementById('nav-title-2').innerHTML = 'games.eu';
+document.getElementById('nav-home').innerHTML = 'Home Page ew Home';
+document.getElementById('nav-shop').innerHTML = 'Shop';
+document.getElementById('nav-quiz').innerHTML = 'Quiz';
+document.getElementById('nav-author').innerHTML = 'About us';
+
+// ####-SECTION-TOP-####
+
+document.getElementById('top-title').innerHTML = 'Board games';
+document.getElementById('top-text').innerHTML = 'Learn the stories of board games and discover the ones you have not known yet';
+document.getElementById('top-button-text').innerHTML = "Let's start";
+
+
+
+
 window.onload = function () {
 
     // ####-SECTION--NAV-####
 
-    document.getElementById('nav-title-1').innerHTML = 'GRY';
-    document.getElementById('nav-title-2').innerHTML = 'plnanszowe.pl';
-    document.getElementById('nav-home').innerHTML = 'Strona główna';
-    document.getElementById('nav-shop').innerHTML = 'Sklep';
+    document.getElementById('nav-title-1').innerHTML = 'BOARD';
+    document.getElementById('nav-title-2').innerHTML = 'games.eu';
+    document.getElementById('nav-home').innerHTML = 'Home Page ew Home';
+    document.getElementById('nav-shop').innerHTML = 'Shop';
     document.getElementById('nav-quiz').innerHTML = 'Quiz';
-    document.getElementById('nav-author').innerHTML = 'O Autorach';
+    document.getElementById('nav-author').innerHTML = 'About us';
 
     // ####-SECTION-TOP-####
 
-    document.getElementById('top-title').innerHTML = 'Gry planszowe';
-    document.getElementById('top-text').innerHTML = 'Poznaj historie gier planszowych i odkryj te, których jeszcze nie znałeś';
-    document.getElementById('top-button-text').innerHTML = 'Zaczynamy';
+    document.getElementById('top-title').innerHTML = 'Board games';
+    document.getElementById('top-text').innerHTML = 'Learn the stories of board games and discover the ones you have not known yet';
+    document.getElementById('top-button-text').innerHTML = "Let's start";
 
     // ####-SECTION-HISORY-####
 
@@ -30,10 +48,10 @@ window.onload = function () {
     document.getElementById('history-title-article-4').innerHTML = 'Inne gry';
     document.getElementById('history-contents-article-4').innerHTML = 'Rzymianie z kolei mieli tzw. <b>Ludus duodecim scriptorum</b>, grę uznawaną za przodka Backgammona, znanego w Polsce pod nazwą Tryktrak. Niektórzy uważają, że to właśnie do tej gry odnosił się Juliusz Cezar wypowiadając słynne słowa: <i>„Kości zostały rzucone”</i>. Oprócz tego Rzymianie grali również w <b>Tabula Rusolia</b>. Rozgrywka miała miejsce na okrągłej planszy, a celem każdego gracza było ustawienie trzech swoich pionów w jednej linii.';
 
-    // document.getElementById('history-title-article-5').innerHTML = 'Młynek';
-    // document.getElementById('history-contents-article-5').innerHTML = "racając jeszcze do czasów średniowiecza i początków czasównowożytnych to ogólnie rzecz biorąc wiele gier, w którezagrywano się w Europie miało starożytne korzenie. Jedną z takich gier jest młynek, a wspominał o niej sam Szekspir. Gra,której angielska nazwa brzmi Nine Nen's Norris wywodzi swoje korzenie ze starożytnego Rzymu, a możliwe, że nawet z Egiptu. Wiadomo jednak, że jak wielu innym grom starożytnym przypisywano jej wyższe znaczenie, a sama plansza pełniła nawet rolę amuletu. Założenia gry przypominają te znane z Rzymskiej Tabula Rusolia. Dzięki ustawieniu 3 pionów w 1 linii możemy zdjąć z planszy pionprzeciwnika. Przegrywa osoba, która zostanie jedynie z 2 pionamiz początkowych 9.";
+    document.getElementById('history-title-article-5').innerHTML = 'Młynek';
+    document.getElementById('history-contents-article-5').innerHTML = "Biorąc pod uwagę czasy średniowiecza i początki czasów nowożytnych, ogólnie rzecz biorąc można powiedzieć, że wiele gier, w które zagrywano się w Europie miało starożytne korzenie. Jedną z takich gier jest chociażby młynek; o grze tej wspominał nawet sam <b>Szekspir.</b> Pochodzenie tej gry nie jest jednak dokładnie znane, choć wiele badaczy sądzi, że wywodzi się ona ze <b>starożytnego Egiptu.</b> Wiadomo też, że planszę do gry bardzo często wykonywano z torfu.  Jak wielu innym grom starożytnym, nierzadko przypisywano  jej wyższe znaczenie, a sama plansza często pełniła nawet <b>rolę amuletu.</b> Założenia gry przypominają te znane z rzymskiej Tabula Rusolia. Dzięki ustawieniu 3 pionów w 1 linii można było zdjąć z planszy pion przeciwnika. Przegrywała osoba zostająca jedynie z 2 pionami z początkowych 9.";
 
-    
+
     document.getElementById('history-title-article-6').innerHTML = 'Monopoly';
     document.getElementById('history-contents-article-6').innerHTML = "Jeśli chodzi o współczesne tytuły to na pewno trzeba wspomnieć o tzw. <b>„The Landlord's Game”.</b> W grze tej poruszamy się po planszy o kształcie kwadratu, wykupujemy posiadłości, dworce, a ponadto możemy trafić do więzienia. Brzmi znajomo? Oczywiście mowa tutaj o kultowym Monopoly. Gra ta została wymyślona przez <b>Lizzie Magie,</b> jedną z pierwszych projektantek gier plaszowych z USA. Początkowo Monopoly nosiło wcześniej już wspomniany odmienny tytuł. The Landlord's Game w swoim założeniu miało być grą z przekazem. Lizzie była feministką i przeciwniczką różnych praktyk biznesowych. Jej gra miała być materializacją sprzeciwu wobec kapitalizmu. W grze tej można było wcielić się w monopolistę lub antymonopolistę. Monopoly z czasem zyskało na popularności i zaczęło przyciągać uwagę społeczeństwa. Niestety pomysł gry z przekazem zgapił <b>Charles Darrow.</b> Delikatnie zmodyfikował go, po czym sprzedał do <b>Parker Brothers</b> (dzisiejsze Hasbro) w 1935 roku. Gra ta od lat 30-tych stała się prawdziwym fenomenem, który trwa aż do dziś. Jest ona jedną z najbardziej rozpoznawalnych gier planszowych na świecie. Monopoly skończyło jednak tylko jako gra czysto rozrywkowa i obecnie pozbawiona jest swojego pierwotnego przekazu, który dla samej Lizzie Magie miał tak duże znaczenie jak metaforyczny odbiór gier przez starożytnych.";
 
@@ -41,25 +59,25 @@ window.onload = function () {
 
     document.getElementById('game-title-1').innerHTML = 'Szachy';
     document.getElementById('game-contents-1').innerHTML = 'Powstały one w VI w. n.e. w Indiach. Do Europy trafiły w okolicach VIII/IX w. i osiadły tam na dobre. Na ich fenomen trzeba było jeszcze wtedy sporo poczekać bo aż do XVI w. Wtedy to wprowadzono kilka istotnych zmian w rozgrywce, które uatrakcyjniły całość i przyczniły się do popularyzacji tejże gry.';
-    
+
     document.getElementById('game-title-2').innerHTML = 'Scrable';
     document.getElementById('game-contents-2').innerHTML = ' Gra ta została wynaleziona przez architekta Alfreda Buttsa w 1931r., chociaż zaczęto ją sprzedawać dopiero w roku 1946. Polega ona na losowaniu płytek z literami i układaniu ich na planszy w słowa. Z czasem gra zaczęła coraz bardziej zyskiwać na popularności, w wyniku czego na całym świecie sprzedano ponad 100 mln egzemplarzy.';
-    
+
     document.getElementById('game-title-3').innerHTML = 'Chińczyk';
     document.getElementById('game-contents-3').innerHTML = 'Stara hinduska gra planszowa pachisi jest znana w Indiach do dziś. Josef Schmidt upraszczając znacznie tę grę stworzył chińczyka znanego nam we współczesnej postaci. Rozgrywka polega na dojściu wszystkimi pionkami do tzw. „domku”. Począwszy od pierwszego wydania w 1914r. na całym świecie zostało sprzedane jakieś 70 mln egzemplarzy.';
-    
+
     document.getElementById('game-title-4').innerHTML = 'Dixit';
     document.getElementById('game-contents-4').innerHTML = ' Stosunkowa nowa gra wydana w 2008 roku przez wydawnictwo Libellud.  Szczególną sławę zdobyła ona w 2010 roku kiedy to wygrała nagrodę „Spiel des Jahres” (gra roku). W założeniu Dixit jest grą w skojarzenia. Punkt zdobywamy wtedy gdy przynajmniej jedna osoba nie zgadnie, o który obrazek z karty nam chodzi.';
-    
+
     document.getElementById('game-title-5').innerHTML = 'Rummikub';
     document.getElementById('game-contents-5').innerHTML = 'Historia Rummikuba sięga jakieś 70 lat wstecz. Została ona zaprojektowana przez Efraima Hertzano. Mężczyzna wymyślił, aby zastąpić tradycyjne karty używane w popularnej grze, kolorowymi płytkami. Gra ta w krótkim czasie stała się wielkim hitem. Celem gry jest jak najszybsze wyłożenie wszystkich kostek z tablicy na stół.';
-    
+
     document.getElementById('game-title-6').innerHTML = 'Warcaby';
     document.getElementById('game-contents-6').innerHTML = 'Warcaby historią swoją sięgają starożytnego Egiptu, lecz znane nam w obecnej formie powstały na Półwyspie Iberyjskim na przełomie XV/XVI w. Podobnie jak w szachach mamy tutaj planszę posiadającą 64 pola. Wygrywa osoba, która pierwsza zbije wszystkie 12 pionów przeciwnika.';
-    
+
     document.getElementById('game-title-7').innerHTML = 'Osadnicy z Catanu';
     document.getElementById('game-contents-7').innerHTML = 'Przełomowa gra wydana niespełna 25 lat temu. Została ona zaprojektowana przez Klausa Teubera. Polega na pozyskiwaniu surowców, zakładaniu osad i miast oraz umiejętnym handlu z innymi graczami. Zwycięzcą jest osoba, która zdobędzie 10 pkt. dzięki wyżej wymienionym czynnościom.';
-    
+
     document.getElementById('game-title-8').innerHTML = 'Wsiąść do pociągu';
     document.getElementById('game-contents-8').innerHTML = 'Gra została zaprojektowana i wydana przez Alana R. Moona w 2004r. W tym samym roku otrzymała tytuł „Najlepszej gry planszowej”. Polega ona na łączeniu tras kolejowych na mapie Stanów Zjednoczonych. Wygrywa osoba, która wyczerpie wszystkie pionki którymi dysponuje.';
 
@@ -87,7 +105,7 @@ window.onload = function () {
     // ####-SETION-BOTTOM-####
 
     document.getElementById('bottom-title-1').innerHTML = 'O projekcie';
-    document.getElementById('bottom-text-1').innerHTML = 'Celem jest zwycięstwo, jest to cel ważny ale a nie wygrany sam w sobie.'; //orginalny ang cytat: The goal is to win, but it is the goal that is important, not the winning. 
+    document.getElementById('bottom-text-1').innerHTML = 'The goal is to win, but it is the goal that is important, not the winning.';
     document.getElementById('bottom-title-2').innerHTML = 'Współpraca';
     document.getElementById('bottom-text-2').innerHTML = 'Kontakt';
     document.getElementById('bottom-text-3').innerHTML = 'O autorach';
