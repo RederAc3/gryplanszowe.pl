7f3b1e6e51e5ee369bfe0448f79fd22d5571a863
Andrzej Kot <reder.ac3@gmail.com>
2019-03-26 00:04:24 +0100

    Bug fixed language and details elements shop

--- build/js/shop.js
+++ build/js/shop.js
@@ -1,29 +1,29 @@
-var princeEl = document.querySelectorAll('.prince-number');
-var buyEl = document.querySelectorAll('.buy-button');
-var titleEl = document.querySelectorAll('.title-product');
+var prince = document.querySelectorAll('.prince-number');
+var buy = document.querySelectorAll('.buy-button');
+var title = document.querySelectorAll('.title-product');
 var modal = document.getElementById('modal');
 var scrollTop = document.documentElement.scrollTop;
 console.log(scrollTop);
 // WYWALA NON STOP NaN NWM CO Z TYM ZROBIĆ
 // var allPrince = 0;
 var getInfo = function() {
-    for ( var i = 0; i <= buyEl.length; i++) {
+    for ( var i = 0; i <= buy.length; i++) {
         var i = i;
-        buyEl[i].addEventListener('click', function() {
+        buy[i].addEventListener('click', function() {
             // console.log(title[i].innerText);
-            var Prince = this.previousElementSibling.innerText.replace('zł', '');
-            var Title = this.nextElementSibling.innerText.trim();
-            var parsePrince = parseInt(Prince);
-            var allPrince = parsePrince ;
+            var addPrince = this.previousElementSibling.innerText.replace('zł', '');
+            var addTitle = this.nextElementSibling.innerText.trim();
+            // var parsePrince = parseInt(addPrince, allPrince);
+            // var allPrince = parsePrince ;
 
-            allPrince = allPrince + parsePrince ; 
+            // allPrince = allPrince + parsePrince ; 
             
             // console.log(typeof(allPrince), allPrince);
             console.log(this.previousElementSibling.innerText.replace('zł', ''));
             console.log(this.nextElementSibling.innerText.trim());
 
             modal.classList.remove('modal-none');
-            modal.innerHTML = '<div id="exit"></div>' + '<p>Dodano do koszyka: ' + Title + '</p>' + Prince + '<p>Razem: ' + allPrince + 'zł </p>';
+            modal.innerHTML = '<div id="exit"></div>' + '<p>Dodano do koszyka: ' + addTitle + '</p>' + addPrince + '<p>Razem: ' + typeof(allPrince) +'zł </p>';
 
             var exitModal = document.getElementById('exit');
 
@@ -31,13 +31,12 @@ var getInfo = function() {
                 modal.classList.add('modal-none');
             });
 
-            // console.log(!modal.classList.contains('modal-none'));
-
+            if (modal.classList.contains('modal-none') = true) {
                 setTimeout(function() {
                     modal.classList.add('modal-none');
-                    console.log(window.navigator.languages);
                 }, 3000);
                 // return allPrince;
+            }
         });
     }
 };
